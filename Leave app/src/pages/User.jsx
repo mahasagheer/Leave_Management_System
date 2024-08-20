18036242ddc3872b61135207f7b569b0465c67db
mahasagheer <mahasagheer960@gmail.com>
2024-08-20 12:25:58 -0700

    search added

--- Leave app/src/pages/User.jsx
+++ Leave app/src/pages/User.jsx
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../service/authentication";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserPlus, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUserPlus,
+  faPenToSquare,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 import Deleteuser from "../components/deleteuser";
 const User = () => {
   const { data, isAdmin, isHR } = useContext(AuthContext);
@@ -12,6 +16,7 @@ const User = () => {
   const local = localStorage.getItem("user");
   const apiURL = import.meta.env.VITE_API;
   const [Loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
 
   const fetchUsers = () => {
     setLoading(true);
@@ -53,26 +58,48 @@ const User = () => {
             {Loading && <div className=" loader ml-[50%] mt-[25%]"></div>}
             {!Loading && (
               <div className="relative overflow-x-auto shadow-md sm:rounded-lg p-3">
-                <span className="flex flex-col sm:flex-row justify-between items-center sm:items-baseline py-4 px-4 sm:px-6">
-                  {isAdmin ? (
-                    <h1 className="text-2xl md:text-3xl sm:text-xl py-4 sm:py-6">
-                      Viewing All Employees ({users.length - 1})
-                    </h1>
-                  ) : (
-                    <h1 className="text-2xl sm:text-3xl py-4 sm:py-6">
-                      Viewing All Employees ({filter.length})
-                    </h1>
-                  )}
-                  {(isAdmin || isHR) && (
-                    <Link
-                      to="/new_user"
-                      className="mt-4  sm:mt-0 text-black h-10 ease-in-out duration-300 transition bg-[#90d7f5] hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-4 sm:px-5 py-2.5 text-center flex items-center justify-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    >
-                      <FontAwesomeIcon icon={faUserPlus} />
-                      <div className="ml-3 whitespace-nowrap">Add Employee</div>
-                    </Link>
-                  )}
-                </span>
+                <div className="flex flex-col lg:flex-row md:flex-col justify-between sm:flex-col items-center py-4 px-4 sm:px-6">
+                  <div className="w-full lg:w-auto">
+                    {isAdmin ? (
+                      <h1 className="text-xl sm:text-2xl md:text-3xl py-4 sm:py-6 text-center lg:text-left">
+                        Viewing All Employees ({users.length - 1})
+                      </h1>
+                    ) : (
+                      <h1 className="text-xl sm:text-2xl md:text-3xl py-4 sm:py-6 text-center lg:text-left">
+                        Viewing All Employees ({filter.length})
+                      </h1>
+                    )}
+                  </div>
+                  <div className="flex flex-col lg:flex-row md:flex-row sm:flex-col md:gap-4 sm:gap-2 justify-center lg:items-center lg:justify-end items-center w-full lg:w-auto">
+                    <div className="relative z-0 w-full lg:w-auto mb-2 ">
+                      <div class="relative  pt-2">
+                        <div class="absolute  inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                          <FontAwesomeIcon
+                            icon={faMagnifyingGlass}
+                            className="mt-2"
+                          />
+                        </div>
+                        <input
+                          type="text"
+                          class="bg-gray-50 border border-gray-300 outline-none text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          onChange={(e) => setSearch(e.target.value)}
+                          placeholder="Search..."
+                        />
+                      </div>
+                    </div>
+                    <div className="w-full lg:w-auto lg:ml-4 mt-2 sm:mt-0">
+                      <Link
+                        to="/new_user"
+                        className="text-black h-10 ease-in-out duration-300 transition bg-[#90d7f5] hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-4 sm:px-5 py-2.5 text-center flex items-center justify-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      >
+                        <FontAwesomeIcon icon={faUserPlus} />
+                        <div className="ml-3 whitespace-nowrap">
+                          Add Employee
+                        </div>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
 
                 <div className="overflow-x-auto">
                   <table className=" text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -115,55 +142,61 @@ const User = () => {
                       </tr>
                     </thead>
 
-                    {filter.map((data) => {
-                      return (
-                        <tbody key={data._id}>
-                          <tr className="border-b border-gray-200 dark:border-gray-700">
-                            <td className="px-7 py-7 bg-gray-50 font-bold underline text-sky-600 dark:bg-gray-800">
-                              <Link to={`/user/${data._id}`}>{data._id}</Link>
-                            </td>
-                            <td className="px-6 py-4">{data.name}</td>
-                            <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                              {data.Job_title}
-                            </td>
-                            <td className="px-6 py-4">{data.gender}</td>
-                            <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                              {data.age}
-                            </td>
-                            <td className="px-6 py-4">
-                              {data.hire_date.substring(0, 10)}
-                            </td>
-                            <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                              {data.salary}
-                            </td>
-                            <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                              {data.department}
-                            </td>
-                            <td className="px-6 py-4">{data.city}</td>
-                            <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                              {data.exit_date}
-                            </td>
-                            <td className="px-6 py-4">{data.email}</td>
-                            <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                              <span className="flex gap-10 justify-center flex-nowrap items-center">
-                                <Link to={`/update_user/${data._id}`}>
-                                  <button className="flex items-center  gap-2 text-blue-700">
-                                    <FontAwesomeIcon
-                                      icon={faPenToSquare}
-                                      size="lg"
-                                    />
-                                  </button>
-                                </Link>
-                                <Deleteuser
-                                  id={data._id}
-                                  onDelete={() => handleUserDelete(data._id)}
-                                />
-                              </span>
-                            </td>
-                          </tr>
-                        </tbody>
-                      );
-                    })}
+                    {filter
+                      .filter((item) => {
+                        return search.toLowerCase() === ""
+                          ? item
+                          : item.name.toLowerCase().includes(search);
+                      })
+                      .map((data) => {
+                        return (
+                          <tbody key={data._id}>
+                            <tr className="border-b border-gray-200 dark:border-gray-700">
+                              <td className="px-7 py-7 bg-gray-50 font-bold underline text-sky-600 dark:bg-gray-800">
+                                <Link to={`/user/${data._id}`}>{data._id}</Link>
+                              </td>
+                              <td className="px-6 py-4">{data.name}</td>
+                              <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                                {data.Job_title}
+                              </td>
+                              <td className="px-6 py-4">{data.gender}</td>
+                              <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                                {data.age}
+                              </td>
+                              <td className="px-6 py-4">
+                                {data.hire_date.substring(0, 10)}
+                              </td>
+                              <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                                {data.salary}
+                              </td>
+                              <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                                {data.department}
+                              </td>
+                              <td className="px-6 py-4">{data.city}</td>
+                              <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                                {data.exit_date}
+                              </td>
+                              <td className="px-6 py-4">{data.email}</td>
+                              <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                                <span className="flex gap-10 justify-center flex-nowrap items-center">
+                                  <Link to={`/update_user/${data._id}`}>
+                                    <button className="flex items-center  gap-2 text-blue-700">
+                                      <FontAwesomeIcon
+                                        icon={faPenToSquare}
+                                        size="lg"
+                                      />
+                                    </button>
+                                  </Link>
+                                  <Deleteuser
+                                    id={data._id}
+                                    onDelete={() => handleUserDelete(data._id)}
+                                  />
+                                </span>
+                              </td>
+                            </tr>
+                          </tbody>
+                        );
+                      })}
                   </table>
                 </div>
               </div>

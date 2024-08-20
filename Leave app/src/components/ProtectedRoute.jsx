edd67787aa45fa4c4cd967492c4caeb095b2e9fb
mahasagheer <mahasagheer960@gmail.com>
2024-08-19 17:50:56 -0700

    recent commit

--- Leave app/src/components/ProtectedRoute.jsx
+++ Leave app/src/components/ProtectedRoute.jsx
@@ -3,17 +3,20 @@ import { useContext } from "react";
 import { AuthContext } from "../service/authentication";
 
 const ProtectedRoute = ({ children, roles }) => {
+  const store = localStorage.getItem("user");
+  const data = JSON.parse(store);
+  const token = data?.token;
   const location = useLocation();
   const from = location.state?.from?.pathname || "/";
 
   const { isUser, isAdmin, isHR } = useContext(AuthContext);
-  if (isAdmin == true && roles.includes("admin")) {
+  if (isAdmin == true && roles.includes("admin") && token) {
     return children;
   }
-  if (isUser == true && roles.includes("user")) {
+  if (isUser == true && roles.includes("user") && token) {
     return children;
   }
-  if (isHR == true && roles.includes("HR")) {
+  if (isHR == true && roles.includes("HR") && token) {
     return children;
   }
   return <Navigate to="/" state={{ from: location }} replace />;

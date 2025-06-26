f32f8dab845072c77a4ee9ffcb90df16081c1935
Daniyal Ayyaz <daniyalayyaz86@gmail.com>
2025-06-26 19:30:56 +0500

    Theme fixing

--- Leave app/src/main.jsx
+++ Leave app/src/main.jsx
@@ -4,13 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./service/authentication.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./context/themeContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+    <ThemeProvider>
       <AuthProvider>
         <App />
       </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
